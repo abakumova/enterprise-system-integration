a57aeb5040873f7fe965deb6a08efd8a2f4aef15
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2023-05-21 19:39:57 +0300

    Week 11.1: Spring Boot Security - Auth-service JWT

---
+++ security/week11-1/ESI2023-week11-1/README.md
@@ -0,0 +1,3 @@
+# ESI2023 - Spring Boot Security
+
+Session 11.1: Spring Boot Security - Auth-service JWT
---
+++ security/week11-1/ESI2023-week11-1/src/main/java/com/esi/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.esi.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		DatabaseInitializer.initialize("usercredentials_db");
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+}
